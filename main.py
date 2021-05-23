061b4e5f06ee978c83a18650df261031293785a7
omegamg05111101 <omegarapid123456789@gmail.com>
2021-05-24 01:53:44 +0530

    added files and updated main

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import discord
-from discord.ext import commands ,tasks
+import os
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.ext.commands import Context
 from discord import Member
-
+from dotenv import load_dotenv
+load_dotenv()
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+#pip install -r requirements.txt
 #client(bot)
 client = commands.Bot(command_prefix='!')
 #version of bot
    
 @client.command(name='ver')
-
 async def ver(context):
     
     myEmbed1=discord.Embed(title="title1", description="description 1",color=0xe91e63)
@@ -21,13 +27,6 @@ async def cmd(context):
     myEmbed2 = discord.Embed(title="title2", description="description 2",color=0x1abc9c)
     myEmbed2.add_field(name="Version2", value="2.4",inline=False)
     myEmbed2.add_field(name="Version Number", value="2.4.5",inline=False)
-    myEmbed2.add_field(name="Version2", value="2.4",inline=False)
-    myEmbed2.add_field(name="Version2", value="2.4",inline=False)
-    myEmbed2.add_field(name="Version2", value="2.4",inline=False)
-    myEmbed2.add_field(name="Version2", value="2.4",inline=False)
-    myEmbed2.add_field(name="Version2", value="2.4",inline=False)
-    myEmbed2.add_field(name="Version2", value="2.4",inline=False)
-    myEmbed2.add_field(name="Version2", value="2.4",inline=False)
 
     await context.message.reply(embed=myEmbed2)
-client.run('ODQyMDYwNTc3ODE0NjA5OTIw.YJv0Bg.gAbxF10kdBLjkW3YTv6Bp5xtcxs')
+client.run(BOT_TOKEN)
